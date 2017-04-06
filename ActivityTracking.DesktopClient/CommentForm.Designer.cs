30383273aca561f6758821aac2d8dcb8e2602a41
AlexGitHub19 <sasha_tkk@mail.ru>
2017-04-06 12:36:21 +0300

    Desktop application added

---
+++ ActivityTracking.DesktopClient/CommentForm.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace ActivityTracking.DesktopClient
+{
+    partial class CommentForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.OkButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 32);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(435, 150);
+            this.textBox1.TabIndex = 0;
+            // 
+            // OkButton
+            // 
+            this.OkButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.OkButton.Location = new System.Drawing.Point(159, 188);
+            this.OkButton.Name = "OkButton";
+            this.OkButton.Size = new System.Drawing.Size(139, 29);
+            this.OkButton.TabIndex = 1;
+            this.OkButton.Text = "OK";
+            this.OkButton.UseVisualStyleBackColor = true;
+            this.OkButton.Click += new System.EventHandler(this.OkButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(160, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Leave your comment:";
+            // 
+            // CommentForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(455, 220);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.OkButton);
+            this.Controls.Add(this.textBox1);
+            this.Name = "CommentForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CommentForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button OkButton;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ ActivityTracking.DesktopClient/QuestionForm.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ActivityTracking.DesktopClient
+{
+    public partial class QuestionForm : Form
+    {
+        Timer timer;
+        UserActivityHook hook;
+        public Timer Timer
+        {
+            get { return timer; }
+        }
+
+        public QuestionForm()
+        {
+            InitializeComponent();
+            TopMost = true;
+            hook = new UserActivityHook();
+            hook.KeyDown += HookKeyDown;
+            hook.OnMouseActivity += HookMouseActivity;
+
+            timer = new Timer();
+            timer.Interval = 5000;
+            timer.Start();
+            timer.Tick += timerTick;
+        }
+
+        private void HookMouseActivity(object sender, MouseEventArgs e)
+        {
+            timer.Stop();
+            timer.Start();
+        }
+
+        private void HookKeyDown(object sender, KeyEventArgs e)
+        {
+            timer.Stop();
+            timer.Start();
+        }
+
+        private void timerTick(object sender, EventArgs e)
+        {
+            ShowForm();
+            timer.Stop();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            notifyIcon1.Icon = Properties.Resources.lines;
+            HideForm();
+        }
+         
+
+        private void notifyIcon1_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Normal;
+            this.ShowInTaskbar = true;
+        }  
+
+         private void ReasonButton_Click(object sender, EventArgs e)
+        {
+            HideForm();
+            timer.Start();
+        }
+
+        private void ShowForm()
+        {
+            this.WindowState = FormWindowState.Normal;
+            this.ShowInTaskbar = true;
+        }
+        private void HideForm()
+        {
+            this.WindowState = FormWindowState.Minimized;
+            this.ShowInTaskbar = false;
+
+        }
+
+        private void OtherButton_Click(object sender, EventArgs e)
+        {
+            timer.Stop();
+            HideForm();
+            CommentForm comentForm = new CommentForm(this);
+            comentForm.Show();
+        }
+    }
+}
