2dbdfa711d257b2422f0c7896f7f3e3d9d7efcb6
AlexGitHub19 <sasha_tkk@mail.ru>
2017-04-11 14:57:06 +0300

    123

--- ActivityTracking.WebClient(WebAPI)/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
+++
@@ -1,9 +0,0 @@
-namespace ActivityTracking.WebClient_WebAPI_.Areas.HelpPage.ModelDescriptions
-{
-    public class KeyValuePairModelDescription : ModelDescription
-    {
-        public ModelDescription KeyModelDescription { get; set; }
-
-        public ModelDescription ValueModelDescription { get; set; }
-    }
-}
--- ActivityTracking.WebClient(WebAPI)/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace ActivityTracking.WebClient_WebAPI_.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
