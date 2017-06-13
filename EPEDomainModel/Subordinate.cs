4cd619360d6e9407284c34e7a3cb03b3e445bc35
AleksMorozova <aleks_morozova91@mail.ru>
2017-06-13 14:59:55 +0300

    Add EPEModel

---
+++ EPEDomainModel/Subordinate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPEModel
+{
+    public class Subordinate
+    {
+        public string Login { get; }
+        public string FirstName { get; }
+        public string LastName { get; }
+    }
+}
